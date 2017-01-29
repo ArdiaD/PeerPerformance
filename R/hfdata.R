bfa594c6387ce485a7c3215ce17a72723cc9aa42
ardiad <david.ardia.ch@gmail.com>
2017-01-29 13:32:15 +0100

    + CRAN version

--- R/hfdata.R
+++ R/hfdata.R
@@ -1,9 +1,9 @@
 #' @name hfdata
 #' @title Hedge fund data
-#' @description The matrix \code{hfdata} contains randomized monthly returns of hedge funds.
-#' @source \url{https://www.hedgefundresearch.com/}
+#' @description The matrix \code{hfdata} contains randomized and modified monthly returns of hedge funds.
+#' @source HFR database
 #' @docType data
 #' @keywords datasets
 #' @usage data(hfdata)
-#' @format A matrix of size 60 x 100, containing 60 monthly net returns for 100 hedge funds. Returns have been slightly modified.
+#' @format A matrix of size 60 x 100, containing 60 monthly net returns for 100 hedge funds. Returns have been modified.
 NULL
